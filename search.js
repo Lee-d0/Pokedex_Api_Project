05e90e7d88626aadc1b52b35e915607c67abf2ad
Lee-d0 <marmar11137@gmail.com>
2024-07-25 10:54:34 -0400

    Api Project

---
+++ search.js
@@ -0,0 +1,33 @@
+const inputElement = document.querySelector("#search-input")
+const search_icon = document.querySelector("#search-close-icon")
+const sort_wrapper = document.querySelector(".sort__wrapper")
+
+
+inputElement.addEventListener("input", () => {
+    handleInputChange(inputElement)
+})
+search_icon.addEventListener("click", handleSearchCloseOnClick)
+sort_wrapper.addEventListener("click", handleSortIconOnClick)
+
+
+function handleInputChange(inputElement){
+    const inputValue = inputElement.value
+
+    if (inputValue !== ""){
+        document.querySelector("#search-close-icon").classList.add("search-close-icon-visible")
+    }else{
+        document.querySelector("#search-close-icon").classList.remove("search-close-icon-visible")
+    }
+}
+
+
+function handleSearchCloseOnClick(){
+    document.querySelector("#search-input").value = ""
+    document.querySelector("#search-close-icon").classList.remove("search-close-icon-visible")
+}
+
+function handleSortIconOnClick(){
+    document.querySelector(".filter__wrapper").classList.toggle("filter__wrapper-open")
+
+    document.querySelector("body").classList.toggle("filter__wrapper-overlay")
+}
